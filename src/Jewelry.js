6339d8bc6a5e8af73c1f040e0ece051e27379acd
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-09-27 22:40:07 +0200

    Update Jewelry.js

--- src/Jewelry.js
+++ src/Jewelry.js
@@ -16,7 +16,7 @@ const showTextClick= (element)=>{
         
             <img className="bigger" src={image} alt="foto" width="400px" height="400px"/>
             <h3>{name}</h3>           
-            <h4>$ {price}</h4>
+            <h4> € {price}</h4>
             <p> {showMore ? description : description.substring(0, 40)} </p>
             <button className="seeMore" onClick={()=>showTextClick(element)}>{ showMore ? "Ver menos" : "Ver más"}</button>
         
