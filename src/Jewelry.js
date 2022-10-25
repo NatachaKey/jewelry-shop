fbd1327f520926b5861acb7c6d644d0a412afbc4
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-25 21:08:15 +0200

    Update Jewelry.js

--- src/Jewelry.js
+++ src/Jewelry.js
@@ -11,14 +11,14 @@ const showTextClick= (element)=>{
     return(
     <div className="products">
     {password.map((element=>{
-        const {id, name, price, image,  description, showMore}= element;
-        return(<div className="productCard" key={id}>  
+        const { id, name, price, image,  description, showMore }= element;
+        return(<div className="productCard" key={ id }>  
         
-            <img className="bigger" src={image} alt="foto" width="380px" height="380px"/>
-            <h3>{name}</h3>           
-            <h4> € {price}</h4>
-            <p> {showMore ? description : description.substring(0, 40)} </p>
-            <button className="seeMore" onClick={()=>showTextClick(element)}>{ showMore ? "Ver menos" : "Ver más"}</button>
+            <img className="bigger" src={ image } alt="foto" width="380px" height="380px"/>
+            <h3>{ name }</h3>           
+            <h4> € { price }</h4>
+            <p> { showMore ? description : description.substring(0, 40)} </p>
+            <button className="seeMore" onClick={ ()=> showTextClick(element)}>{ showMore ? "Ver menos" : "Ver más"}</button>
         
     </div>
 
