e10ff0fa35de1f83304f2c5fce25f6929bc9ac88
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-23 22:47:12 +0100

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,6 +1,12 @@
+import { data } from "./data";
+
 function Buttons({ filteredJewelry }){
+    const [products, setProducts] = useState(data);
+    
     return(
         <div className="cont">
+        
+            <button className="change" onClick={ ()=> setProducts(data) }>Ver todo </button>
             <button className="change" onClick={ ()=> filteredJewelry("anillo") }>Anillos </button>
             <button className="change" onClick={ ()=> filteredJewelry("pendientes") }>Pendientes </button>
             <button className="change" onClick={ ()=> filteredJewelry("pulsera") }>Pulseras</button>
@@ -9,3 +15,17 @@ function Buttons({ filteredJewelry }){
 }
 
 export default Buttons;
+
+/*function Buttons({ filteredJewelry }){
+    return(
+        <div className="cont">
+        
+        
+            <button className="change" onClick={ ()=> filteredJewelry("anillo") }>Anillos </button>
+            <button className="change" onClick={ ()=> filteredJewelry("pendientes") }>Pendientes </button>
+            <button className="change" onClick={ ()=> filteredJewelry("pulsera") }>Pulseras</button>
+        </div>
+    )
+}
+
+export default Buttons;*/
