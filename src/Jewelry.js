2fb611e00586f487e919307343d7fe8c91ddda8e
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-23 22:15:19 +0100

    Update Jewelry.js

--- src/Jewelry.js
+++ src/Jewelry.js
@@ -19,7 +19,7 @@ const showTextClick= (element)=>{
             <h4> € { price }</h4>
             <p> { showMore ? description : description.substring(0, 40)} </p>
             <button className="seeMore" onClick={ ()=> showTextClick(element)}>{ showMore ? "Ver menos" : "Ver más"}</button>
-            <button className="infoBtn"> <a href="test@google.com" className="infoBtn" > Pedir cita </a></button>
+            
      )
     }
 ))}
