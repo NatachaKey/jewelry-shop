f77aaf945c8efe2c5289b41c24eaf7f01685956a
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-11-23 22:52:08 +0100

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -17,16 +17,3 @@ function Buttons({ filteredJewelry }){
 
 export default Buttons;
 
-/*function Buttons({ filteredJewelry }){
-    return(
-        <div className="cont">
-        
-        
-            <button className="change" onClick={ ()=> filteredJewelry("anillo") }>Anillos </button>
-            <button className="change" onClick={ ()=> filteredJewelry("pendientes") }>Pendientes </button>
-            <button className="change" onClick={ ()=> filteredJewelry("pulsera") }>Pulseras</button>
-        </div>
-    )
-}
-
-export default Buttons;*/
