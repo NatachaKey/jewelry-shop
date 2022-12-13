1d7ceec86ad27a3115518fa3e14778dd065c72f7
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-12-13 17:02:20 +0100

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -7,7 +7,7 @@ function Buttons({ filteredJewelry }){
     return(
         <div className="cont">
         
-            <button className="change" onClick={ ()=> setProducts(data)}>Ver todo </button>
+            <button className="change" onClick={ ()=> products(data)}>Ver todo </button>
             <button className="change" onClick={ ()=> filteredJewelry("anillo") }>Anillos </button>
             <button className="change" onClick={ ()=> filteredJewelry("pendientes") }>Pendientes </button>
             <button className="change" onClick={ ()=> filteredJewelry("pulsera") }>Pulseras</button>
